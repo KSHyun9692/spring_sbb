f6440d981e2070c3e05dffa2c21d307d43e4b57a
KSHyun9692 <KSHyun9692@gmail.com>
2025-12-03 23:48:04 +0900

    게시판 프로젝트

---
+++ sbb/src/main/java/com/mysite/sbb/QuestionRepository.java
@@ -0,0 +1,16 @@
+package com.mysite.sbb;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface QuestionRepository extends JpaRepository<Question,Integer> {
+
+    //제목으로 찾기
+    Question findBySubject(String subject);
+
+    //제목과 내용 둘 다 찾기
+    Question findBySubjectAndContent(String subject, String content);
+
+    List<Question> findBySubjectLike(String subject);
+}
